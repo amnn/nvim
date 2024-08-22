0f45c5a079e777f52562bd0bffb43c4b8ed69189
Ashok Menon <ashok@mystenlabs.com>
2024-08-22 23:34:18 +0100

    fix(treesitter): Configure nvim-treesitter
    - Enables relevant grammars
    - Enables auto-install
    - Enables treesitter-based highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,36 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = "*",
-    opts = {},
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "c",
+          "clojure",
+          "fennel",
+          "fish",
+          "graphql",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "rust",
+          "scheme",
+          "sql",
+          "tsx",
+          "typescript",
+          "vim",
+        },
+
+        modules = {},
+        ignore_install = {},
+        sync_install = false,
+        auto_install = true,
+
+        highlight = {
+          enable = true,
+        },
+      }
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
