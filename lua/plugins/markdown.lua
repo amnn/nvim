ff5eb34c49d12a905563e49dae4b26e17b7438d6
Ashok Menon <ashok@mystenlabs.com>
2026-04-04 17:55:56 +0100

    markdown: checkbox cycling
    Add shortcuts for cycling through checkbox states in the nearest
    containing checkbox list item.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -41,6 +41,95 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+
+      local cycle = { "[ ]", "[/]", "[x]" }
+      local pattern = "^(%s*)([^%[%s]+%s+)(%[[^%]]%])(.*)$"
+
+      local function find_checkbox(buf, cursor)
+        local ok, node = pcall(vim.treesitter.get_node, {
+          bufnr = buf,
+          pos = { cursor[1] - 1, cursor[2] },
+          lang = "markdown",
+        })
+
+        if not ok then return nil end
+        while node and node:type() ~= "list_item" do
+          node = node:parent()
+        end
+
+        if not node then return nil end
+
+        local start = node:range()
+        local line = vim.api.nvim_buf_get_lines(buf, start, start + 1, false)[1]
+
+        if not line then return nil end
+
+        local indent, marker, state, text = line:match(pattern)
+        if not indent then return nil end
+        return start, indent, marker, state, text
+      end
+
+      local function set_checkbox(state)
+        local bufnr = vim.api.nvim_get_current_buf()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local line, indent, marker, _, text = find_checkbox(bufnr, cursor)
+        if not line then return end
+
+        local updated = table.concat { indent, marker, state, text }
+
+        vim.api.nvim_buf_set_lines(bufnr, line, line + 1, false, { updated })
+        vim.api.nvim_win_set_cursor(0, cursor)
+      end
+
+      local function cycle_checkbox()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local line, indent, marker, state, text = find_checkbox(bufnr, cursor)
+        if not line then return end
+
+        local curr = vim.fn.index(cycle, state)
+        local next = cycle[(curr + 1) % #cycle + 1]
+        local updated = table.concat { indent, marker, next, text }
+
+        vim.api.nvim_buf_set_lines(bufnr, line, line + 1, false, { updated })
+        vim.api.nvim_win_set_cursor(0, cursor)
+      end
+
+      local function dispatch_action()
+        local action = vim.fn.getcharstr()
+
+        if action == "d" then
+          set_checkbox "[x]"
+        elseif action == "x" then
+          set_checkbox "[-]"
+        elseif action == "w" then
+          set_checkbox "[^]"
+        elseif action == "i" then
+          set_checkbox "[!]"
+        else
+          while action == "c" do
+            cycle_checkbox()
+            action = vim.fn.getcharstr(0)
+            if action == "" then return end
+          end
+
+          vim.api.nvim_feedkeys(vim.keycode(action), "n", false)
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.keymap.set("n", "<LocalLeader>c", dispatch_action, {
+            buffer = event.buf,
+            desc = "[C]heckbox",
+            silent = true,
+          })
+        end,
+      })
+    end,
     opts = {
       checkbox = {
         enabled = true,
